f73522f2e8b7dc2a4aed656e2a427c718a9d38e5
scottrobinson9116 <58603544+scottrobinson9116@users.noreply.github.com>
2025-06-29 09:33:33 -0500

    Add files via upload

---
+++ currentOrders/page.tsx
@@ -0,0 +1,131 @@
+"use client"
+
+import styles from "./page.module.css"
+import Link from "next/link"
+import { useState, useEffect, useRef } from 'react'
+import axios from 'axios'
+import https from 'https'
+
+//Establishes a connection with the backend Node.js server
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:2001',
+  timeout: 10000,
+  httpsAgent: new https.Agent({ keepAlive: true }),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+//Interface for each ticket and array of tickets
+interface Item {
+  name: string,
+  price: number,
+  type: string,
+  _id: string,
+}
+interface Ticket {
+  dateCreated: string;
+  orderItems: Item[];
+  orderNumber: string,
+  totalPayment: number,
+  status: string,
+}
+
+const CurrentOrders: React.FC = () => {
+  const [pendingTickets, setPendingTickets] = useState<Ticket[]>([])
+  const [showTickets, setTicketBool] = useState<boolean>(false)
+  const [loadingCounter, setLoading] = useState<number>(0)
+
+  useEffect(() => {
+    //Creates and executes loading element
+    const interval = setInterval(() => {
+      setLoading(0)
+      const interval2 = setInterval(() => {
+        setLoading(prev => prev + 1)
+      }, 500)
+      setTimeout(() => {
+        clearInterval(interval2)
+      }, 1500)
+    }, 1500)
+
+    setTimeout(() => {
+      clearInterval(interval)
+    }, 4500)
+
+    setTimeout(() => {
+      setTicketBool(true)
+    }, 6500)
+
+    //Axios function that gets all pending tickets
+    async function getTickets(): Promise<void> {
+      try {
+        const { data: response } = await axiosInstance.get('/tickets/pendingtickets');
+        const pendingDataArr: Ticket[] = await response;
+        setPendingTickets(pendingDataArr)
+      }
+      catch (err) {
+        console.log("Unable to Fetch Menu", err);
+      }
+    }
+    getTickets();
+  }, [])
+
+  //Handles the onClick complete button by patching the current ticket with "complete" and removing it from view
+  async function completeTicket(id: number): Promise<void> {
+    try {
+      const ticketId: object = {
+        id: id,
+      }
+
+      const { data: response } = await axiosInstance.patch('/tickets/status/complete', ticketId);
+      const pendingDataArr: Ticket[] = await response;
+      setPendingTickets(pendingDataArr)
+     
+    }
+    catch (err) {
+      console.log("Unable to Fetch Menu", err);
+    }
+  }
+
+  //Complete handler
+  function handleComplete(id: number) {
+    completeTicket(id)
+  }
+
+  return (
+    <div>
+      {!showTickets && <div className={styles.loading}>
+        {loadingCounter === 0 && <h1>Loading...</h1>}
+        {loadingCounter === 1 && <h1>Loading.</h1>}
+        {loadingCounter === 2 && <h1>Loading..</h1>}
+        {loadingCounter === 3 && <h1>Loading...</h1>}
+      </div>}
+      {showTickets &&
+        <div>
+          <div className={styles.ticketMenu }>
+            <button><Link href="/">Menu</Link></button>
+            <button><Link href="/history">History</Link></button>
+          </div>
+        <ul className={styles.ticketlist}>
+        <div className={styles.ticket }>
+          {pendingTickets.map((obj) => (
+            <li className={styles.ticketcontent} key={obj.orderNumber}>
+            <h1>Order: {obj.orderNumber}</h1>
+              <p>{obj.dateCreated}</p>
+              <button onClick={() => handleComplete(obj._id) }>Complete</button>
+              <ul>
+                <div className={styles.line}></div>
+              {obj.orderItems.map((obj2, index) => (
+                <li className={styles.items} key={index}>{obj2.name}</li>
+              ))}
+              </ul>
+            </li>
+        ))}
+        </div>
+        </ul>
+        </div>}
+    </div>
+)
+}
+
+export default CurrentOrders;
---
+++ history/page.tsx
@@ -0,0 +1,136 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import styles from "./page.module.css"
+import Link from 'next/link'
+import axios from 'axios'
+import https from "https"
+
+//Establishes a connection with the backend Node.js server
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:2001',
+  timeout: 10000,
+  httpsAgent: new https.Agent({ keepAlive: true }),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const History: React.FC = () => {
+
+  //State
+  const [allTickets, setAllTickets] = useState<Ticket[]>([])
+  const [popupBool, setPopupBool] = useState<boolean>(false)
+  const [idState, setIdState] = useState<string>("")
+  const [orderState, setOrderState] = useState<number>(0)
+  const [dateState, setDateState] = useState<string>()
+  const [itemState, setItemState] = useState<Item[]>([])
+  const [totalState, setTotalState] = useState<number>(0)
+  const [statusState, setStatusState] = useState<string>("")
+
+  //Types for ticket objects and arrays
+  type Ticket = {
+    _id: string,
+    orderNumber: number;
+    dateCreated: string;
+    orderItems: Item[];
+    totalPayment: number;
+    status: string;
+  }
+
+  type Item = {
+    _id: string,
+    name: string,
+    price: number,
+    type: string,
+  }
+
+  useEffect(() => {
+    //Fetches every order in MongoDB
+    async function getAllOrders(): void {
+      const { data: response } = await axiosInstance.get('/tickets/all');
+      const pendingDataArr: Ticket[] = await response;
+      setAllTickets(pendingDataArr);
+    }
+    getAllOrders()
+  }, [])
+
+  //Conditionally renders ticket info and buttons when a ticket is clicked
+  function handlePopup(index: number): void {
+    const selectedOrder = allTickets[index]
+    setIdState(selectedOrder._id)
+    setOrderState(selectedOrder.orderNumber)
+    setDateState(selectedOrder.dateCreated)
+    setItemState(selectedOrder.orderItems)
+    setTotalState(selectedOrder.totalPayment)
+    setStatusState(selectedOrder.status)
+    setPopupBool(true)
+  }
+  //Set ticket as refund and removes it from currentOrders page
+  async function refund(idState: string) {
+    const { data: response } = await axiosInstance.patch('/tickets/status/refund', { idState });
+    const pendingDataArr: Ticket[] = await response;
+    setAllTickets(pendingDataArr)
+    setStatusState("refund")
+  }
+  //Resets ticket to pending and becomes visible on currentOrders page
+  async function remake(idState: string) {
+    const { data: response } = await axiosInstance.patch('/tickets/status/pendingOrder', { idState });
+    const pendingDataArr: Ticket[] = await response;
+    setAllTickets(pendingDataArr)
+    setStatusState("pending")
+  }
+
+  //Handles onClick for each ticket button
+  function handleRefund() {
+    refund(idState)
+  }
+  function handleRemake() {
+    remake(idState)
+  }
+
+  return (
+    <div className={styles.pagewrapper}>
+      <div className={styles.header}>
+      <h1>Order History</h1>
+      <div className={styles.ticketmenu}>
+        <button><Link href="/">Menu</Link></button>
+        <button><Link href="/currentOrders">Tickets</Link></button>
+        </div>
+      </div>
+    <div className={styles.orderlist}>
+        {allTickets.map((obj, index) => (
+          <button onClick={() => handlePopup(index)} key={index} ><div className={styles.order}>
+          <p className={styles.boldtext}>Order: </p><p>{obj.orderNumber}</p>
+          <p className={styles.boldtext}>Date: </p><p>{obj.dateCreated}</p>
+          <p className={styles.boldtext}>Total: </p><p>${obj.totalPayment}</p>
+        </div></button>
+      ))}
+      </div>
+      {popupBool &&
+        <div className={styles.popup}>
+          <div className={styles.popupcontent}>
+            <h2>Order: {orderState}</h2>
+            <h3>Date: </h3>
+            <p>{dateState}</p>
+            <h3>Items: </h3>
+            <ul className={styles.itemslist }>
+            {itemState.map((obj, index) => (
+              <li key={index}>{obj.name}</li>
+            ))}
+            </ul>
+            <h3>Total: </h3>
+            <p>${totalState}</p>
+            <h3>Status: </h3>
+            <p>{statusState}</p>
+            <div className={styles.ticketbuttons}>
+              <button onClick={() => handleRefund(idState) }>Refund</button>
+              <button onClick={() => handleRemake() }>Remake</button>
+            </div>
+          </div>
+        </div>}
+    </div>
+  )
+}
+
+export default History;
